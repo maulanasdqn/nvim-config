94ba03e6ff1ff62d2bf4fa589c263cdf31316b79
Maulana Sodiqin <sodiqincahyana1@gmail.com>
2024-04-21 22:58:23 +0700

    feat: update

--- lua/plugins/indent.lua
+++ lua/plugins/indent.lua
@@ -1,11 +1,27 @@
 return {
 	"lukas-reineke/indent-blankline.nvim",
 	event = { "BufReadPre", "BufNewFile" },
+	opts = {
+		indent = {
+			char = "│",
+			tab_char = "│",
+		},
+		scope = { enabled = false },
+		exclude = {
+			filetypes = {
+				"help",
+				"alpha",
+				"dashboard",
+				"neo-tree",
+				"Trouble",
+				"trouble",
+				"lazy",
+				"mason",
+				"notify",
+				"toggleterm",
+				"lazyterm",
+			},
+		},
+	},
 	main = "ibl",
-	enabled = false,
-
-	config = function()
-		local indent = require("ibl")
-		indent.setup()
-	end,
 }
